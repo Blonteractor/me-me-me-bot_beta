3e6b54b4bc60b6fc199066512dafa499e9757167
Blonteractor <48821041+Blonteractor@users.noreply.github.com>
2020-04-07 10:37:33 +0530

    some bug fixes, bot now in a running state

--- scripts/bot/cogs/utility.py
+++ scripts/bot/cogs/utility.py
@@ -102,7 +102,7 @@ class Utility(commands.Cog):
 
     #* CLEAR
     @commands.command()
-    @commands.has_permission(manage_messages=True)
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=10):
         '''I can delete the evidence of a bully, no one shall know.'''
 
